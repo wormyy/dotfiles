d308826942f44888c4b703f9f6bd73c78578d9c9
Július Retzer <jul.retzer@gmail.com>
2017-11-08 20:25:07 +0100

    Add iterm settings

--- iterm/setup.fish
+++ iterm/setup.fish
@@ -1,6 +1,6 @@
 #! /usr/bin/fish
 
 # Specify the preferences directory
-defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$(realpath ./itermSettings)"
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$(realpath .)"
 # Tell iTerm2 to use the custom preferences in the directory
 defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
