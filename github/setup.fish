5764235a88f1ed2a8cf00a57c5b6a26721f49ed6
Július Retzer <jul.retzer@gmail.com>
2019-03-31 00:11:37 +0100

    repos

--- github/setup.fish
+++ github/setup.fish
@@ -1,6 +1,6 @@
 #! /usr/bin/fish
 
 ssh-keygen -t rsa -b 4096 -C "jul.retzer@gmail.com"
-eval "$(ssh-agent -s)"
+eval ssh-agent -s
 ssh-add -K ~/.ssh/id_rsa
 pbcopy < ~/.ssh/id_rsa.pub
--- repos/setup.sh
+++ repos/setup.sh
@@ -3,17 +3,32 @@
 DIR=$(dirname "$0")
 cd "$DIR"
 
+. ../scripts/functions.sh
+
 COMMENT=\#*
 REPO_PATH=$(realpath ~/repos)
 
 find * -name "*.list" | while read fn; do
     folder="${fn%.*}"
+
+    info "Cloning $folder repositories..."
+
+    substep_info "Creating $folder folder..."
     mkdir -p "$REPO_PATH/$folder"
     while read repo; do
         if [[ $repo == $COMMENT ]];
         then continue; else
-            repo_name=$(basename $repo .git)
-            git clone $repo "$REPO_PATH/$folder/$repo_name"
+            pushd "$REPO_PATH/$folder" &> /dev/null
+            git clone $repo &> /dev/null
+            if [[ $? -eq 128 ]]; then
+                substep_success "$repo already exists."
+            elif [[ $? -eq 0 ]]; then
+                substep_success "Cloned $repo."
+            else
+                substep_error "Failed to clone $repo."
+            fi
+            popd &> /dev/null
         fi
     done < "$fn"
+    success "Finished cloning $folder repositories."
 done
